b8afc7ed45e771804b7d2f3c3de42768d0f51b90
Marvindlls <marvindillenseger@gmail.com>
2025-02-06 09:22:00 +0100

    Add ApiResource annotation to Doctor entity for API exposure

--- src/Entity/Doctor.php
+++ src/Entity/Doctor.php
@@ -4,8 +4,10 @@ namespace App\Entity;
 
 use App\Repository\DoctorRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
 #[ORM\Entity(repositoryClass: DoctorRepository::class)]
+#[ApiResource]
 class Doctor
 {
     #[ORM\Id]
